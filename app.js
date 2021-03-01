3772b8f17bd7faab5ad55a776b6da7551120d6ca
Eric-Arthur <sh00ter666@live.de>
2021-03-01 18:16:47 +0100

    Update app.js
    Adding document ready memes to make sure the ajax loading bit doesn't execute before the page is ready? .... I guess?

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var folder = "covers/"; 
 
-$.ajax({
-    url : folder,
-    success: function (data) {
-        $(data).find("a").attr("href", function (i, val) {
-            if( val.match(/\.(jpe?g|png|gif)$/) ) { 
-                $(".gallery").append( "<img src='"+ folder + val +"'>" );
-            } 
-        });
-    }
+$(document).ready( function () {
+
+    $.ajax({
+        url : folder,
+        success: function (data) {
+            $(data).find("a").attr("href", function (i, val) {
+                if( val.match(/\.(jpe?g|png|gif)$/) ) { 
+                    $(".gallery").append( "<img src='"+ folder + val +"'>" );
+                } 
+            });
+        }
+    });
+    
 });
